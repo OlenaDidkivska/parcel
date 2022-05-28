0aac6fb7bbd5cb53e4b2ef9ab452fcfa76a1b28e
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-05-28 12:33:36 +0300

    add photo and menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const body = document.querySelector(".page-body")
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -12,5 +13,23 @@ import './sass/main.scss';
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle("no-scroll");
   });
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector(".page-header__btn-open-menu"),
+//     closeMenuBtn: document.querySelector(".page-header__btn-close-menu"),
+//     menu: document.querySelector(".page-header__menu"),
+//     body: document.querySelector("body"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-open");
+//     refs.body.classList.toggle("no-scroll");
+//   }
+// })();
